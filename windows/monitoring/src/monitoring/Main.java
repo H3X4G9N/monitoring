8fff8f27646105dee111d7ad54e7cddc87f901ca
H3X4G9N <30376065+H3X4G9N@users.noreply.github.com>
2019-10-31 17:19:58 +0200

    Update Main.java

--- windows/monitoring/src/monitoring/Main.java
+++ windows/monitoring/src/monitoring/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
@@ -12,6 +13,11 @@ import javafx.scene.control.Separator;
 import javafx.scene.control.Hyperlink;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+import java.io.FileInputStream; 
+import java.io.FileNotFoundException; 
+import javafx.scene.layout.GridPane;
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -19,12 +25,13 @@ public class Main extends Application {
     }
 	
 	@Override
-	public void start(Stage stage) throws Exception {
+	public void start(Stage stage) throws FileNotFoundException {
 		VBox signInVB = new VBox();
-		Scene signInS = new Scene(signInVB);
-		
 		VBox signUpVB = new VBox();
 		VBox resetPasswordVB = new VBox();
+		VBox dashboardVB = new VBox();
+		
+		Scene scene = new Scene(signInVB);
 		
 		{
 			Label emailL = new Label("Email");
@@ -40,6 +47,14 @@ public class Main extends Application {
 			signInVB.getChildren().add(passwordPF);
 			
 			Button signInB = new Button("Sign in");	
+			
+			signInB.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                	scene.setRoot(dashboardVB);
+                }
+            });
+			
 			signInVB.getChildren().add(signInB);
 			
 			Separator separator1 = new Separator();
@@ -50,7 +65,7 @@ public class Main extends Application {
 			signUpHL.setOnAction(new EventHandler<ActionEvent>() {
                 @Override
                 public void handle(ActionEvent event) {
-                	signInS.setRoot(signUpVB);
+                	scene.setRoot(signUpVB);
                 }
             });
 			
@@ -61,7 +76,7 @@ public class Main extends Application {
 			resetPasswordUpHL.setOnAction(new EventHandler<ActionEvent>() {
                 @Override
                 public void handle(ActionEvent event) {
-                	
+                	scene.setRoot(resetPasswordVB);
                 }
             });
 			
@@ -87,8 +102,16 @@ public class Main extends Application {
 			PasswordField passwordConfirmationPF = new PasswordField();
 			signUpVB.getChildren().add(passwordConfirmationPF);
 			
-			Button signInB = new Button("Sign up");
-			signUpVB.getChildren().add(signInB);
+			Button signUpB = new Button("Sign up");
+			
+			signUpB.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                	scene.setRoot(dashboardVB);
+                }
+            });
+			
+			signUpVB.getChildren().add(signUpB);
 			
 			Separator separator1 = new Separator();
 			signUpVB.getChildren().add(separator1);
@@ -98,7 +121,7 @@ public class Main extends Application {
 			signUpHL.setOnAction(new EventHandler<ActionEvent>() {
                 @Override
                 public void handle(ActionEvent event) {
-                	signInS.setRoot(signInVB);
+                	scene.setRoot(signInVB);
                 }
             });
 			
@@ -106,10 +129,10 @@ public class Main extends Application {
 			
 			Hyperlink resetPasswordUpHL = new Hyperlink("Reset password");
 			
-			 resetPasswordUpHL.setOnAction(new EventHandler<ActionEvent>() {
+			resetPasswordUpHL.setOnAction(new EventHandler<ActionEvent>() {
                 @Override
                 public void handle(ActionEvent event) {
-                	
+                	scene.setRoot(resetPasswordVB);
                 }
             });
 			
@@ -125,11 +148,99 @@ public class Main extends Application {
 			
 			Button signInB = new Button("Reset password");
 			resetPasswordVB.getChildren().add(signInB);
+			
+			Separator separator1 = new Separator();
+			resetPasswordVB.getChildren().add(separator1);
+			
+			Hyperlink signInHL = new Hyperlink("Sign in");
+			
+			signInHL.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                	scene.setRoot(signInVB);
+                }
+            });
+			
+			resetPasswordVB.getChildren().add(signInHL);
+			
+			Hyperlink signUpHL = new Hyperlink("Sign up");
+			
+			signUpHL.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                	scene.setRoot(signUpVB);
+                }
+            });
+			
+			resetPasswordVB.getChildren().add(signUpHL);
 		}
 		
+		{
+			HBox unnamed1 = new HBox();
+			dashboardVB.getChildren().add(unnamed1);
+			
+
+			Image logoI = new Image(getClass().getResource("/res/img/logo.png").toExternalForm());
+			Image dashboardI = new Image(getClass().getResource("/res/img/dashboard.png").toExternalForm());
+			Image serverI = new Image(getClass().getResource("/res/img/server.png").toExternalForm());
+			Image dataCollectorsI = new Image(getClass().getResource("/res/img/data_collectors.png").toExternalForm());
+			Image settingsI = new Image(getClass().getResource("/res/img/settings.png").toExternalForm());
+
+			ImageView logoIV = new ImageView();
+			logoIV.setFitWidth(32);
+			logoIV.setFitHeight(32);
+			logoIV.setImage(logoI);
+			
+			unnamed1.getChildren().add(logoIV);
+			
+			Label companyName = new Label("Monitoring");
+			unnamed1.getChildren().add(companyName);
+			
+			HBox unnamed2 = new HBox();
+			dashboardVB.getChildren().add(unnamed2);
+			
+			VBox unnamed3 = new VBox();
+			unnamed2.getChildren().add(unnamed3);
+			
+			Button dashboardB = new Button("Dashboard");
+			unnamed3.getChildren().add(dashboardB);
+			
+			ImageView dashboardIV = new ImageView();
+			dashboardIV.setImage(dashboardI);
+			dashboardIV.setFitWidth(32);
+			dashboardIV.setFitHeight(32);
+			dashboardB.setGraphic(dashboardIV);
+			
+			Button serverB = new Button("Server");
+			unnamed3.getChildren().add(serverB);
+			
+			ImageView serverIV = new ImageView();
+			serverIV.setImage(serverI);
+			serverIV.setFitWidth(32);
+			serverIV.setFitHeight(32);
+			serverB.setGraphic(serverIV);
+			
+			Button dataCollectorsB = new Button("Data collectors");
+			unnamed3.getChildren().add(dataCollectorsB);
+			
+			ImageView dataCollectorsIV = new ImageView();
+			dataCollectorsIV.setImage(dataCollectorsI);
+			dataCollectorsIV.setFitWidth(32);
+			dataCollectorsIV.setFitHeight(32);
+			dataCollectorsB.setGraphic(dataCollectorsIV);
+			
+			Button settingsB = new Button("Settings");
+			unnamed3.getChildren().add(settingsB);
+			
+			ImageView settingsIV = new ImageView();
+			settingsIV.setImage(settingsI);
+			settingsIV.setFitWidth(32);
+			settingsIV.setFitHeight(32);
+			settingsB.setGraphic(settingsIV);
+		}
 		
 		stage.setTitle("Monitoring");
-		stage.setScene(signInS);
+		stage.setScene(scene);
 		stage.show();
 	}
 }
